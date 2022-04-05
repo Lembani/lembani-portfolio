f5d282e2d7c3a06bf004bc968bd635971fd74c3b
Lembani Sakala <lembanisakala@gmail.com>
2022-04-05 17:53:03 +0200

    Update to fix linter issues: 1. Call the close method in Javascript. 2. Use Id's to select nav links

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
-let navMenu = document.querySelector(".nav");
-let menuContainer = document.querySelector(".menu");
-let brand = document.querySelector(".brand");
-let mobileNavItems = document.querySelector(".mobile-nav-items");
-let mobileNavItem = document.querySelectorAll(".mobile-nav-item");
-let menuIcon = document.querySelector(".menu-icon");
-let closeIcon = document.querySelector(".close-menu-icon");
+const navMenu = document.querySelector('.nav');
+const menuContainer = document.querySelector('.menu');
+const brand = document.querySelector('.brand');
+const mobileNavItems = document.querySelector('.mobile-nav-items');
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-menu-icon');
+const porfolio = document.getElementById('portfolio-link');
+const about = document.getElementById('about-link');
+const contact = document.getElementById('contact-link');
 
-mobileNavItems.style.display = "none";
-closeIcon.style.display = "none";
+mobileNavItems.style.display = 'none';
+closeIcon.style.display = 'none';
 
 const menuToggle = () => {
-	if (navMenu.classList.contains("displayMenu")) {
-		navMenu.classList.remove("displayMenu");
-		closeIcon.style.display = "none";
-		menuIcon.style.display = "block";
-		brand.style.display = "block";
-		mobileNavItems.style.display = "none";
-	} else {
-		navMenu.classList.add("displayMenu");
-		closeIcon.style.display = "block";
-		mobileNavItems.setAttribute("class", "mobile-nav-items");
-		menuIcon.style.display = "none";
-		brand.style.display = "none";
-		mobileNavItems.style.display = "block";
-	}
+  if (navMenu.classList.contains('displayMenu')) {
+    navMenu.classList.remove('displayMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+    brand.style.display = 'block';
+    mobileNavItems.style.display = 'none';
+  } else {
+    navMenu.classList.add('displayMenu');
+    closeIcon.style.display = 'block';
+    mobileNavItems.setAttribute('class', 'mobile-nav-items');
+    menuIcon.style.display = 'none';
+    brand.style.display = 'none';
+    mobileNavItems.style.display = 'block';
+  }
 };
 
-menuContainer.addEventListener("click", menuToggle);
-function closeMenu(e) {
-	closeIcon.style.display = "none";
-	menuIcon.style.display = "block";
-	brand.style.display = "block";
-	mobileNavItems.style.display = "none";
+menuContainer.addEventListener('click', menuToggle);
+function closeMenu() {
+  closeIcon.style.display = 'none';
+  menuIcon.style.display = 'block';
+  brand.style.display = 'block';
+  mobileNavItems.style.display = 'none';
 }
+
+porfolio.addEventListener('click', closeMenu);
+about.addEventListener('click', closeMenu);
+contact.addEventListener('click', closeMenu);
