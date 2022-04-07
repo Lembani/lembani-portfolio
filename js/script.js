631dff11f9d97e38989127e3e3ca1deff59aa5e7
Lembani Sakala <lembanisakala@gmail.com>
2022-04-07 15:33:47 +0200

    Update to dynamically render work cards

--- js/script.js
+++ js/script.js
@@ -39,3 +39,111 @@ function closeMenu() {
 porfolio.addEventListener('click', closeMenu);
 about.addEventListener('click', closeMenu);
 contact.addEventListener('click', closeMenu);
+
+const projects = [
+  {
+    projectTitle: 'Multi-Post Stories Gain+Glory',
+    projectImg: './images/img-placeholder.png',
+    projectCategories: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnText: 'See Project',
+    projectDetails: {
+      name: 'Keeping track of hundreds of components',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      featuredImage: '',
+      technologies: ['CodeKit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'CodePen'],
+      liveVersionLink: '',
+      sourceLink: '',
+    },
+  },
+  {
+    projectTitle: 'Multi-Post Stories Gain+Glory',
+    projectImg: './images/img-placeholder.png',
+    projectCategories: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnText: 'See Project',
+    projectDetails: {
+      name: 'Keeping track of hundreds of components',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      featuredImage: '',
+      technologies: ['CodeKit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'CodePen'],
+      liveVersionLink: '',
+      sourceLink: '',
+    },
+  },
+  {
+    projectTitle: 'Multi-Post Stories Gain+Glory',
+    projectImg: './images/img-placeholder.png',
+    projectCategories: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnText: 'See Project',
+    projectDetails: {
+      name: 'Keeping track of hundreds of components',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      featuredImage: '',
+      technologies: ['CodeKit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'CodePen'],
+      liveVersionLink: '',
+      sourceLink: '',
+    },
+  },
+  {
+    projectTitle: 'Multi-Post Stories Gain+Glory',
+    projectImg: './images/img-placeholder.png',
+    projectCategories: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnText: 'See Project',
+    projectDetails: {
+      name: 'Keeping track of hundreds of components',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      featuredImage: '',
+      technologies: ['CodeKit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'CodePen'],
+      liveVersionLink: '',
+      sourceLink: '',
+    },
+  },
+  {
+    projectTitle: 'Multi-Post Stories Gain+Glory',
+    projectImg: './images/img-placeholder.png',
+    projectCategories: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnText: 'See Project',
+    projectDetails: {
+      name: 'Keeping track of hundreds of components',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      featuredImage: '',
+      technologies: ['CodeKit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'CodePen'],
+      liveVersionLink: '',
+      sourceLink: '',
+    },
+  },
+  {
+    projectTitle: 'Multi-Post Stories Gain+Glory',
+    projectImg: './images/img-placeholder.png',
+    projectCategories: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnText: 'See Project',
+    projectDetails: {
+      name: 'Keeping track of hundreds of components',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      featuredImage: '',
+      technologies: ['CodeKit', 'GitHub', 'JavaScript', 'Bootstrap', 'Terminal', 'CodePen'],
+      liveVersionLink: '',
+      sourceLink: '',
+    },
+  },
+];
+
+const workCardContainer = document.querySelector('.work-card-container');
+projects.forEach((proj, index) => {
+  let catItems = '';
+  proj.projectCategories.map((catItem) => {
+    catItems += `<li class="cat-item">${catItem}</li>`;
+    return null;
+  });
+
+  const card = `
+  <div class="work-card">
+    <img class="work-card-img" src="${proj.projectImg}" alt="Multi-Post Stories Gain plus Glory" />
+    <div class="work-card-content">
+      <h3 class="card-title">${proj.projectTitle}</h3>
+      <ul class="categories">${catItems}</ul>
+    <button class="see-proj-btn" type="button" id="${index}" name="see-project" aria-label="See project button">See Project</button>
+  </div>
+ </div>
+ `;
+  workCardContainer.innerHTML += card;
+});
